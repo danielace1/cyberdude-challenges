416c24f6bab447428f7dbac3d34696f1900673b2
Sudharsan <danielsudharsan1805@gmail.com>
2024-01-12 15:18:46 +0530

    feat: display yt-thumbnail in ui

--- javascript/02-javascript-DOM/05-yt-thumbnail-viewer/assets/js/main.js
+++ javascript/02-javascript-DOM/05-yt-thumbnail-viewer/assets/js/main.js
@@ -1,26 +1,29 @@
 const urlFormEl = document.querySelector("#urlForm");
 const thumbnailImgEl = document.querySelector("#thumbnailImg");
-const urlInputEl = document.querySelector("#urlInput");
 
-const videoURL = `https://www.youtube.com/watch?v=Fh9YgIT1mPc`;
-
-const videoId = videoURL.substring("32");
-
-const getUrl = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
-
-// https://www.youtube.com/watch?v=Fh9YgIT1mPc
-// https://i.ytimg.com/vi/[VIDEO_ID]/maxresdefault.jpg
 urlFormEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (urlInputEl.value === "") {
-  } else {
+  const urlInputEl = document.querySelector("#urlInput");
+  const urlInputElVal = urlInputEl.value;
+
+  // https://www.youtube.com/watch?v=Fh9YgIT1mPc
+  // https://www.youtube.com/watch?v=ZQkUYmaVwgs&list=RDCLAK5uy_ksEjgm3H_7zOJ_RHzRjN1wY-_FFcs7aAU
+
+  const videoId = urlInputElVal.slice(32, 43);
+  const videoId2 = urlInputElVal.slice(49);
+
+  const getUrl = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
+
+  const validUrl = `https://www.youtube.com/watch?v=${videoId}`;
+  const validUrl2 = `https://www.youtube.com/watch?v=${videoId}&list=${videoId2}`;
+
+  if (urlInputEl === "") {
+  } else if (urlInputElVal === validUrl || urlInputElVal === validUrl2) {
     thumbnailImgEl.src = getUrl;
-    thumbnailImgEl.alt = urlInputEl.value;
+    thumbnailImgEl.alt = videoId;
     thumbnailImgEl.classList.remove("hidden", "w-10", "h-10", "mx-auto");
     thumbnailImgEl.classList.add("px-40", "py-3", "rounded");
+  } else {
+    alert("Enter Youtube video URL only!");
   }
-
-  const inputUrl = e.target[0].value;
-
-  console.log(inputUrl);
 });
